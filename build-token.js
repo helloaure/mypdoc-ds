6e3bd99febbd5cba515f1bed6c0e7e95e70dda35
helloaure <helloaure@gmail.com>
2026-06-25 18:16:41 +0200

    Create build-token.js

---
+++ build-token.js
@@ -0,0 +1,22 @@
+import StyleDictionary from 'style-dictionary';
+// Ici, on a bien remplacé par "register" tout court
+import { register } from '@tokens-studio/sd-transforms';
+
+// Ici aussi, on utilise "register"
+register(StyleDictionary);
+
+const sd = new StyleDictionary({
+  source: ['tokens/tokens.json'], 
+  platforms: {
+    css: {
+      transforms: ['attribute/cti', 'name/kebab', 'ts/color/css/hexrgba', 'ts/size/px'], 
+      buildPath: 'src/styles/',
+      files: [{
+        destination: 'variables.css',
+        format: 'css/variables'
+      }]
+    }
+  }
+});
+
+await sd.buildAllPlatforms();
